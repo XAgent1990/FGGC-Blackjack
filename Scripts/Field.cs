59456d1bf4a7d1e77bc5dc4a30ad4a46a0781b2b
Puma008 <markus.sandmann-9999@web.de>
2024-10-23 01:38:21 +0200

    Added Mouse Toggle

--- Scripts/Field.cs
+++ Scripts/Field.cs
@@ -3,37 +3,52 @@ using static FGGCBlackJack.Utility;
 using Godot;
 using static Godot.GD;
 using System.Collections.Generic;
+using System;
 
 public partial class Field : Node3D {
-    int players = 8;
-    List<Hand> hands = new List<Hand>();
-    Deck deck;
+	int players = 1;
+	List<Hand> activeHands = new List<Hand>();
+	Deck deck;
 
-    public Field(int playerAmount, int deckSize) {
-        players = playerAmount;
-        for (int x = 0; x < players + 1; x++) {
-            hands.Add(Hand.NewInstance);
-        }
-        deck = new Deck((byte)deckSize);
-    }
 
-    public void BeginRound() {
-        //Print(deck);
-        for (int iCards = 0; iCards < 2; iCards++) {
-            for (int x = 0; x < players + 1; x++) {
-                if (x == 0 && iCards == 0) {
-                    //dealer karte verdeckt halten
-                }
-                hands[x].AddCard(deck.Draw_Card());
-                //delay
-            }
-        }
-        string text = $"";
-        foreach (Hand h in hands) {
-            text += $"{h}\n";
-        }
-        Print(text);
-        Print(deck);
-    }
+	public enum DeckSettings {
+		Random,
+		Deck
+	}
+	public Field(int playerAmount, int deckSize) {
+		players = playerAmount;
+		for (int x = 0; x < players + 1; x++) {
+			activeHands.Add(Hand.NewInstance);
+		}
+		deck = new Deck((byte)deckSize);
+	}
 
+	public override void _Ready() {
+		BeginRound();
+	}
+	public void BeginRound() {
+		//Print(deck);
+		for (int iCards = 0; iCards < 2; iCards++) {
+			for (int x = 0; x < players + 1; x++) {
+				if (x == 0 && iCards == 0) {
+					//dealer karte verdeckt halten
+				}
+				activeHands[x].AddCard(deck.Draw_Card());
+				//delay
+			}
+		}
+		string text = $"";
+		foreach (Hand h in activeHands) {
+			text += $"{h}\n";
+		}
+		Print(text);
+		Print(deck);
+	}
+
+	public void ResetRound() {
+		if (deck.cards.Count <= 26) {
+			//reset deck
+		}
+		//reset hands
+	}
 }
--- Scripts/PlayerInput.cs
+++ Scripts/PlayerInput.cs
@@ -2,44 +2,65 @@ using Godot;
 using System;
 using static Godot.GD;
 
-public partial class PlayerInput : MultiplayerSynchronizer
-{
-	[Export] public bool boost = false;
-	[Export] public bool ascend = false;
-	[Export] public bool descend = false;
-	[Export] public Vector2 inputDirection = new();
-	[Export] public Vector2 mouseDirection = new();
-	[Export] public bool authorized = false;
+public partial class PlayerInput : MultiplayerSynchronizer {
+    [Export] public bool boost = false;
+    [Export] public bool ascend = false;
+    [Export] public bool descend = false;
+    [Export] public Vector2 inputDirection = new();
+    [Export] public Vector2 mouseDirection = new();
+    [Export] public bool authorized = false;
 
-	private bool changed;
+    bool freeMouse = true;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready(){
-		authorized = GetMultiplayerAuthority() == Multiplayer.GetUniqueId();
-		SetProcess(authorized);
-		if(authorized)
-			Input.MouseMode = Input.MouseModeEnum.Captured;
-	}
+    private bool changed;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta){
-		inputDirection = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
-		boost = Input.IsActionPressed("Boost");
-		ascend = Input.IsActionPressed("Ascend");
-		descend = Input.IsActionPressed("Descend");
-		if(changed)
-			changed = false;
-		else
-			mouseDirection = Vector2.Zero;
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        authorized = GetMultiplayerAuthority() == Multiplayer.GetUniqueId();
+        SetProcess(authorized);
+        if (authorized) {
+            ToggleMouseMode();
+        }
+    }
 
-	public override void _Input(InputEvent @event){
-		base._Input(@event);
-		if(authorized && @event.GetType() == typeof(InputEventMouseMotion)){
-			mouseDirection = ((InputEventMouseMotion)@event).Relative;
-			changed = true;
-		}
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) {
+        if (Input.IsActionJustPressed("ToggleMouseMode")) {
+            ToggleMouseMode();
+        }
+        if (!freeMouse) {
+            inputDirection = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+            boost = Input.IsActionPressed("Boost");
+            ascend = Input.IsActionPressed("Ascend");
+            descend = Input.IsActionPressed("Descend");
+            if (changed)
+                changed = false;
+            else
+                mouseDirection = Vector2.Zero;
+        }
+
+
+
+    }
+
+    void ToggleMouseMode() {
+        if (!freeMouse) {
+            Input.MouseMode = Input.MouseModeEnum.Visible;
+            freeMouse = true;
+        }
+        else {
+            Input.MouseMode = Input.MouseModeEnum.Captured;
+            freeMouse = false;
+        }
+    }
+
+    public override void _Input(InputEvent @event) {
+        base._Input(@event);
+        if (!freeMouse && authorized && @event.GetType() == typeof(InputEventMouseMotion)) {
+            mouseDirection = ((InputEventMouseMotion)@event).Relative;
+            changed = true;
+        }
+    }
 
 
 
